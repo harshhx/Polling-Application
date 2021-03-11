84a9bda1ddffb7daeb9704a52b77f6ce1b146164
harshhx <hg070401@gmail.com>
2021-03-12 01:20:13 +0530

    final version 1.0

--- poll/urls.py
+++ poll/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from poll import views
 
 urlpatterns = [
-    path("", views.home, name= "home"),
+    path("", views.home, name="home"),
     path("create/", views.create, name="create"),
-    path("results/<poll_id>", views.results, name= "results"),
-    path("vote/<poll_id>", views.vote, name= "vote")
+    path("results/<poll_id>", views.results, name="results"),
+    path("vote/<poll_id>", views.vote, name="vote"),
+    path("contact/", views.contact, name="contact")
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import CreatePoll
 from .models import Poll
 
@@ -28,8 +29,32 @@ def create(request):
 
 
 def results(request, poll_id):
-    return render(request, "poll/results.html", {})
+    poll = Poll.objects.get(pk=poll_id)
+    context = {
+        "poll": poll
+    }
+    return render(request, "poll/results.html", context)
 
 
 def vote(request, poll_id):
-    return render(request, "poll/vote.html", {})
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == "POST":
+        selected_option = request.POST["poll"]
+        if selected_option == "option1":
+            poll.option_one_count += 1
+        elif selected_option == "option2":
+            poll.option_two_count += 1
+        elif selected_option == "option3":
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, "Invalid form response!!")
+        poll.save()
+        return redirect('results', poll.id)
+    context = {
+        "poll": poll
+    }
+    return render(request, "poll/vote.html", context)
+
+
+def contact(request):
+    return render(request, "poll/contact.html", {})
